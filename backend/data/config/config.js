5e86c5742021f4445e284977faff627dbab40db2
Vuong <abc@gmail.com>
2025-02-17 10:12:46 +0700

    add production db connect to migrate

--- backend/data/config/config.js
+++ backend/data/config/config.js
@@ -8,5 +8,14 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
-  }
+  },
+
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
 };
